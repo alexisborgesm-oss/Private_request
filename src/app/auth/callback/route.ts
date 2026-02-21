c397b7199a96a9dc7571ff9bd4cc86396dfddb41
alexisborgesm-oss <alexisborgesm@gmail.com>
2026-02-21 14:52:00 -0700

    Update route.ts

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
-import { createServerClient } from "@supabase/ssr";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export async function GET(request: Request) {
   const url = new URL(request.url);
   const code = url.searchParams.get("code");
   const next = url.searchParams.get("next") ?? "/p";
+
   const cookieStore = cookies();
 
   if (code) {
@@ -14,18 +15,19 @@ export async function GET(request: Request) {
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
         cookies: {
-          get(name) {
+          get(name: string) {
             return cookieStore.get(name)?.value;
           },
-          set(name, value, options) {
+          set(name: string, value: string, options: CookieOptions) {
             cookieStore.set({ name, value, ...options });
           },
-          remove(name, options) {
+          remove(name: string, options: CookieOptions) {
             cookieStore.set({ name, value: "", ...options });
-          }
-        }
+          },
+        },
       }
     );
+
     await supabase.auth.exchangeCodeForSession(code);
   }
 
